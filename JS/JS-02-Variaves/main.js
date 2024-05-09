0c0815581cf3f355bc8e1310277aa8bd720ec295
andreluiswebdev <andreluis.h@hotmail.com>
2024-05-08 21:44:29 -0300

    JS-02-Variaveis_exercicioCasa

---
+++ JS/JS-02-Variaves/main.js
@@ -0,0 +1,63 @@
+// objeto console
+console.log('Hello World!')
+// quando trabalho com texto coloco o mesmo dentro de aspas duplas ou simples
+
+// Variaveis
+// quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+
+var _felippe = 8
+_felippe = "Messi"
+
+let letsGo = 4
+letsGo = 6
+
+console.log(letsGo)
+
+
+console.log(_felippe)
+
+console.clear()
+
+// const
+const PI = 3.1415
+
+console.log(PI)
+
+console.clear()
+
+let NAN = 0 / 0
+
+console.log(NAN)
+
+console.clear()
+
+// vai palmeiras
+let IOS = "Instituto da oportunidade social"
+// variavel IOS recebeu uma string
+
+console.log("Instituto " + "da " + "oportunidade " + "social")
+
+console.clear()
+
+var numero1 = 2
+var texto = "Eu tenho "
+var text2 = " Reais"
+
+console.log(texto + numero1 + text2)
+// concatenar = é possivel concatenar strings ou concatenar variaveis
+
+console.clear()
+
+// VALORES BOOLEANOS
+
+var num1 = 1
+var num2 = 1
+// num1 é igual a num2
+// 1 é igual a 1 = true
+// == true or false
+
+console.log(num1 == num2)
+
+var teste;
+
+console.log(teste)
