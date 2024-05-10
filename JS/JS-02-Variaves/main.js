19d8f1a9517a875603dd9ac953d31453f9d8ae7a
andreluiswebdev <andreluis.h@hotmail.com>
2024-05-09 21:33:09 -0300

    JS-02-Variaveis

--- JS/JS-02-Variaves/main.js
+++ JS/JS-02-Variaves/main.js
@@ -61,3 +61,59 @@ console.log(num1 == num2)
 var teste;
 
 console.log(teste)
+
+console.clear()
+
+//  Operadores
+
+var operador1 = 8 
+var operador2 = null
+
+console.log(operador1 * operador2) // 8 * null = 0
+
+var operador3 = "5"
+var operador4 = 1
+
+console.log(operador3 - operador4); // "5" - 1 = 4
+
+var operador5 = "5"
+var operador6 = 1
+
+console.log(operador5 + operador6) // "5" + 1 = 51
+
+var operador7 = "five"
+var operador8 = 2
+
+console.log(operador7 * operador8) // "five" * 2 = NaN
+
+console.clear()
+
+var valor1 = 2
+var valor2 = 2
+var resultado = valor1 % valor2 
+
+console.log(resultado)
+
+console.clear()
+
+var compara = 2 > 3 != 2 
+var teste = !compara
+// != compara se é diferente ou igual
+// => se o valor é maior ou igual
+// <= se o valor é menor ou igual
+// === compara valor e tipo
+// == só está comparando o valor
+
+console.log("variavel compara " + compara)
+console.log("variavel teste " + teste)
+
+console.clear()
+
+let valor3 = 4 
+let valor4 = 6
+let resultado1 = valor3 < valor4 || valor3 == valor4
+// && (E) SE TODOS FOREM TRUE, ENTÃO É TRUE
+// || (AND OU OR) SE UM FOR TRUE, ENTÃO É TRUE
+// ! (NOT OU NÃO) SEMPRE AO CONTRÁRIO 
+
+console.log(resultado1)
