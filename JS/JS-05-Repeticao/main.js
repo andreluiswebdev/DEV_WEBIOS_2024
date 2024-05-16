18184c4b87d217fe3be78e974179d961150c04e0
andreluiswebdev <andreluis.h@hotmail.com>
2024-05-15 21:39:35 -0300

    JS-05-Repeticao

---
+++ JS/JS-05-Repeticao/main.js
@@ -0,0 +1,30 @@
+// WHILE = ENQUANTO
+// Enquanto 0 < 10 vou apresentar a mensagem 0 ainda é menor que 10"
+
+// var contador = 0
+// while(contador <= 10){
+//     console.log(contador + " ainda é menor que 10");
+//     contador++; // contador = contador +1
+//     debugger
+// }
+
+// console.clear();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+// DO WHILE = FAÇA ENQUANTO
+
+// var contador = 0
+
+// do{
+//     debugger
+//     console.log(contador + " ainda é menor que 10 ");
+    
+//     contador++
+// }while(contador < 10)
+
+// console.clear();
+
+
+
+for(var contador = 10; contador < 10; contador < 10 && contador >= 5 || contador ==90; contador ++){
+    console.log(contador + " ainda é menor que 10");
+}
